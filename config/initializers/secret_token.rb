8a45df965003950e622b028259bfa83004e16a74
Eric Hu <erichu@Erics-MacBook-Pro.local>
2013-11-07 00:10:30 -0800

    Initial commit: minimum customizable bourbon neat grid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestProj::Application.config.secret_key_base = '7d844ad4dca5cfbff8bfa3af6bc468b7083197e5288e0d214e3d984b4af7287e4bfcd2d692e22099feea44dc5edca0d21bdf6f178e2f2a8ead855ea603cb2df0'
